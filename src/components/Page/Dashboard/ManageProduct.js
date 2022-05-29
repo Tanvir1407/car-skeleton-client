0b7bde31fc1a9ed6402cfde0eb032f29d13103ed
MD. Tanvir <mt14050@gmail.com>
2022-05-29 19:48:00 +0600

    add product section

--- src/components/Page/Dashboard/ManageProduct.js
+++ src/components/Page/Dashboard/ManageProduct.js
@@ -1,11 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import ManageCard from "./ManageCard";
 const ManageProduct = () => {
-    return (
-        <div>
-            Manage product
-        </div>
-    );
+    
+  const [parts, setParts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5500/parts")
+      .then((res) => res.json())
+      .then((data) => setParts(data));
+  }, []);
+  return (
+    <div className="bg-gray-100 py-8 md:px-12">
+      <h1 className="text-center text-3xl py-16 underline underline-offset-4">
+        Manage Your All Products
+      </h1>
+      <div className=" grid grid-cols-1 sm:mx-3 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {parts.map((part) => (
+          <ManageCard part={part}></ManageCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ManageProduct;
